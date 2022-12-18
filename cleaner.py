2ec5efaed018d6e24c8e0efa892890bf0b007630
louispawaon <lpawaon@gmail.com>
2022-12-18 09:46:24 +0800

    init cleaner - makes all charcters in lowercase - removes all characters after space - removes non-alphanumerics - removes duplicates - overwriting it to the same file

---
+++ cleaner.py
@@ -0,0 +1,29 @@
+#make characters small
+#make remove characters after space 
+#remove non alphanumerics
+
+import string
+
+# Open the file in read mode
+with open('file.txt', 'r') as file:
+  # Read all the lines in the file into a list
+  lines = file.readlines()
+
+# Open the file in write mode
+with open('file.txt', 'w') as file:
+  # Iterate over the lines in the list
+  for line in lines:
+    # Make all letters lowercase
+    line = line.lower()
+    # Remove everything after the first space
+    line = line.split(' ', 1)[0]
+    # Remove all non-alphanumeric characters
+    line = ''.join(c for c in line if c.isalnum())
+    # Split the line into words
+    words = line.split()
+    # Remove any duplicate words
+    words = list(set(words))
+    # Join the words back into a single string
+    line = ' '.join(words)
+    # Write the modified line to the file
+    file.write(line + '\n')
